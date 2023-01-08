e2e68cab02639f2c1c1c66da8d715d73d9c487be
Sudhir1126 <119277926+Sudhir1126@users.noreply.github.com>
2023-01-09 00:17:56 +0530

    Add files via upload

---
+++ Fuel.js
@@ -0,0 +1,11 @@
+function calculateFuel(n){
+    let fuel;
+    if(n<=10){
+        fuel = 100;
+    }
+        else{
+            fuel = (n *10);
+        }
+         console.log(fuel);
+}
+ calculateFuel(19);
