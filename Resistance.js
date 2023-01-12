fae6368e235cf9d29b6ed852c0f47dc6bdbe12b2
Sudhir1126 <119277926+Sudhir1126@users.noreply.github.com>
2023-01-12 13:06:46 +0530

    Add files via upload

---
+++ Resistance.js
@@ -0,0 +1,13 @@
+function seriesResistance(arr) {
+    let Sum = 0;
+    for(let i = 0 ; i < arr.length ; i++ ){
+      Sum += arr[i];
+    }
+    if(Sum > 1){
+      console.log(Sum);
+    }
+    else{
+      console.log(Sum);
+    }
+  }
+  seriesResistance([1,3,6,11]);
