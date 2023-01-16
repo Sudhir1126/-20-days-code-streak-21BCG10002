41561db3e85c1405242d4fedea56abe55074c6e6
Sudhir1126 <119277926+Sudhir1126@users.noreply.github.com>
2023-01-16 11:43:47 +0530

    Add files via upload

---
+++ reversePsychology.js
@@ -0,0 +1,9 @@
+function reversePsychology(s){
+	if (s) {
+	console.log("Do not " + s +".");
+  }
+	else {
+		console.log('Do not do anything.');
+	}
+}
+reversePsychology("wash your mouth after eating");
