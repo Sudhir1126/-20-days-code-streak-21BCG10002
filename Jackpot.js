60a67de822c4e1a6393dc0cf8b01e8cf17341304
Sudhir1126 <119277926+Sudhir1126@users.noreply.github.com>
2023-01-20 11:20:35 +0530

    Add files via upload

---
+++ Jackpot.js
@@ -0,0 +1,12 @@
+function testJackpot(array)
+{
+    if(array[0]===array[1] && array[0]===array[2] && array[0]===array[3])
+
+    console.log(true);
+
+    else
+    {
+    console.log(false);       
+    }
+}
+testJackpot([77,77,77,77]);;
