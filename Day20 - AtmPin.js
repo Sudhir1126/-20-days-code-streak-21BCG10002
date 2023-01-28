4fed61b494ec9dc31789ffa6c9de293228f37748
Sudhir1126 <119277926+Sudhir1126@users.noreply.github.com>
2023-01-28 17:46:45 +0530

    Add files via upload

---
+++ Day20 - AtmPin.js
@@ -0,0 +1,13 @@
+function validatePIN(pin) {
+	if (pin == "") {
+		console.log(false);
+	}
+	if (pin.match(/\d/g).length == 4) {
+		console.log(true);
+	}
+	if (pin.match(/\d/g).length == 6) {
+		console.log(true);
+}
+	console.log(false);
+}
+validatePIN("@,1,3,4");
